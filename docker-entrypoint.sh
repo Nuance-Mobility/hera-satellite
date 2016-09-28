9fac8826905b5b9747c1c43ae4264b573c3c8417
Pete Lyons <peter.lyons@nuance.com>
2016-09-28 13:16:45 -0400

    Changed entrypoint if from -a to -x

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,9 +5,8 @@ export PATH=$PATH:/etc/sensu/plugins:/etc/sensu/handlers
 
 if [ "$1" = 'start' ]; then
 
-    if [ -a /etc/sensu/conf.d/extra-setup.sh ]; then
+    if [ -x /etc/sensu/conf.d/extra-setup.sh ]; then
         echo "Performing extra setup"
-        exec chmod +x /etc/sensu/conf.d/extra-setup.sh
         exec /etc/sensu/conf.d/extra-setup.sh
     fi
     exec /opt/sensu/bin/sensu-client \
