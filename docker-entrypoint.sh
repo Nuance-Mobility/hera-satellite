044b3952931474419b4e8a3961b17c0e4c5ffdc4
Pete Lyons <peter.lyons@nuance.com>
2016-09-28 15:51:21 -0400

    Remove exec on extra-setup

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,7 +7,7 @@ if [ "$1" = 'start' ]; then
 
     if [ -x /etc/sensu/conf.d/extra-setup.sh ]; then
         echo "Performing extra setup"
-        exec /etc/sensu/conf.d/extra-setup.sh
+        /etc/sensu/conf.d/extra-setup.sh
     fi
     exec /opt/sensu/bin/sensu-client \
         -c /etc/sensu/config.json -d /etc/sensu/conf.d \
